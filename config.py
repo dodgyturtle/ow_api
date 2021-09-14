f6cad44ca2885eb1cf3b95ac94813ccb71bc0719
Aleksandr Semenov <semenlinux@gmail.com>
2021-09-14 15:52:39 +0300

    Add expire period to settings

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY = "\2\1thisismyscretkey\1\2\e\y\y\h"
+WEB_TOKEN_PERIOD_EXPIRE_SECONDS=86400
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "test.db")
 # SQLALCHEMY_DATABASE_URI = 'mysql://username:password@mysqlserver.local/quickhowto'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://scott:tiger@localhost:5432/myapp'
 # SQLALCHEMY_ECHO = True
-SQLALCHEMY_POOL_RECYCLE = 3
+# SQLALCHEMY_POOL_RECYCLE = 3
 SQLALCHEMY_TRACK_MODIFICATIONS = True
