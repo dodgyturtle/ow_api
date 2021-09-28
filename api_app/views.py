7a83855a18e43fbd382d382693eb073be59af098
Aleksandr Semenov <semenlinux@gmail.com>
2021-09-28 14:44:09 +0300

    Remove unnecessary  make_response

--- api_app/views.py
+++ api_app/views.py
@@ -3,7 +3,7 @@ from functools import wraps
 from typing import Any, Callable
 
 import jwt
-from flask import Blueprint, jsonify, make_response, request, url_for, wrappers, current_app
+from flask import Blueprint, request, url_for, wrappers, current_app
 from marshmallow import ValidationError
 from . import db
 from .models import Item, User
@@ -20,17 +20,15 @@ def token_required(function: Any) -> Any:
             token = request.headers["x-access-tokens"]
 
         if not token:
-            return make_response(jsonify({"message": "Token is missing"}), 403)
+            return {"message": "Token is missing"}, 403
 
         try:
             data = jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
             current_user = User.query.filter_by(username=data.get("username")).first()
         except jwt.exceptions.InvalidTokenError:
-            return make_response(jsonify({"message": "Token is invalid"}), 403)
+            return {"message": "Token is invalid"}, 403
         if not current_user:
-            return make_response(
-                jsonify({"message": "Тoken does not belong to any user"}), 403
-            )
+            return {"message": "Тoken does not belong to any user"}, 403
         return function(current_user, *args, **kwargs)
 
     return decorator
@@ -38,24 +36,17 @@ def token_required(function: Any) -> Any:
 
 @api_blueprint.app_errorhandler(500)
 def internal_server_error(e):
-    return make_response(jsonify({"message": "Internal Server Error"}), 500)
+    return {"message": "Internal Server Error"}, 500
 
 
 @api_blueprint.app_errorhandler(404)
 def send_url_not_found(e):
-    return make_response(jsonify({"message": "URL not Found"}), 404)
+    return {"message": "URL not Found"}, 404
 
 
 @api_blueprint.app_errorhandler(400)
 def send_bad_request(e):
-    return make_response(
-        jsonify(
-            {
-                "message": "400 Bad Request: The browser (or proxy) sent a request that this server could not understand"
-            }
-        ),
-        400,
-    )
+    return {"message": "400 Bad Request: The browser (or proxy) sent a request that this server could not understand"}, 400
 
 
 @api_blueprint.route("/api/v1/items", methods=["GET"])
@@ -64,7 +55,7 @@ def index(user: User) -> wrappers.Response:
     raw_items = Item.query.filter_by(user_id=user.id).all()
     item_schema = ItemSchema(many=True)
     items = item_schema.dump(raw_items)
-    return make_response(jsonify({"items": items}))
+    return {"items": items}
 
 
 @api_blueprint.route("/api/v1/items/new", methods=["POST"])
@@ -75,12 +66,12 @@ def create_item(user: User) -> wrappers.Response:
     try:
         data = item_schema.load(json_data)
     except ValidationError as err:
-        return make_response(jsonify({"message": f"{ err.messages }"}), 422)
+        return {"message": f"{ err.messages }"}, 422
     item_name = data["name"]
     item = Item(name=item_name, user_id=user.id)
     item.create()
     result = item_schema.dump(Item.query.get(item.id))
-    return make_response(jsonify({"item": result}), 200)
+    return {"item": result}, 200
 
 
 @api_blueprint.route("/api/v1/items/<id>", methods=["DELETE"])
@@ -88,15 +79,13 @@ def create_item(user: User) -> wrappers.Response:
 def delete_item(user: User, id: int) -> wrappers.Response:
     item = Item.query.get(id)
     if not item:
-        return make_response(jsonify({"message": "No item with such id"}), 422)
+        return {"message": "No item with such id"}, 422
     check_user = User.query.filter(User.items.any(Item.id == id)).first()
     if not check_user == user:
-        return make_response(
-            jsonify({"message": "This user can,t delete this item"}), 403
-        )
+        return {"message": "This user can,t delete this item"}, 403
     db.session.delete(item)
     db.session.commit()
-    return make_response(jsonify({"item": f"Item: { item.name } deleted"}), 200)
+    return {"item": f"Item: { item.name } deleted"}, 200
 
 
 @api_blueprint.route("/api/v1/send", methods=["POST"])
@@ -107,24 +96,24 @@ def send_item(user: User) -> wrappers.Response:
     try:
         data = new_user_schema.load(json_data)
     except ValidationError as err:
-        return make_response(jsonify({"message": f"{ err.messages }"}), 422)
+        return {"message": f"{ err.messages }"}, 422
     item_id, new_username = data["item_id"], data["new_username"]
     check_users = User.query.filter_by(username=new_username).all()
     if not check_users:
-        return make_response(jsonify({"message": "No destination user"}), 422)
+        return {"message": "No destination user"}, 422
     check_items = Item.query.filter_by(id=item_id).all()
     if not check_items:
-        return make_response(jsonify({"message": "No item with such id"}), 422)
+        return {"message": "No item with such id"}, 422
     user_items = User.query.filter(
         User.username == user.username, User.items.any(Item.id == item_id)
     ).all()
     if not user_items:
-        return make_response(jsonify({"message": "Item not belong to user"}), 403)
+        return {"message": "Item not belong to user"}, 403
     new_user_items = User.query.filter(
         User.username == new_username, User.items.any(Item.id == item_id)
     ).all()
     if new_user_items:
-        return make_response(jsonify({"message": "User already has this item"}), 422)
+        return {"message": "User already has this item"}, 422
     move_token = jwt.encode(
         {
             "item_id": item_id,
@@ -133,7 +122,7 @@ def send_item(user: User) -> wrappers.Response:
         current_app.config["SECRET_KEY"],
     )
     move_url = url_for(".get_item", move_token=move_token, _external=True)
-    return make_response(jsonify({"move_url": move_url}), 200)
+    return {"move_url": move_url}, 200
 
 
 @api_blueprint.route("/api/v1/get/<move_token>", methods=["GET"])
@@ -145,27 +134,25 @@ def get_item(user: User, move_token: str) -> wrappers.Response:
             move_token, current_app.config["SECRET_KEY"], algorithms=["HS256"]
         )
     except jwt.exceptions.InvalidTokenError:
-        return make_response(jsonify({"message": "Token in url is invalid"}), 422)
+        return {"message": "Token in url is invalid"}, 422
     item_id, new_username = move_token_data.get("item_id"), move_token_data.get(
         "new_username"
     )
     if not user.username == new_username:
-        return make_response(jsonify({"message": "Another user token"}), 403)
+        return {"message": "Another user token"}, 403
     new_user = User.query.filter_by(username=new_username).one()
     item = Item.query.get(item_id)
     if not item:
-        return make_response(jsonify({"message": "Item is not found"}), 422)
+        return {"message": "Item is not found"}, 422
     user_items = User.query.filter(
         User.username == new_username, User.items.any(Item.id == item_id)
     ).all()
     if user_items:
-        return make_response(
-            jsonify({"message": "User already has this item or reuse url"}), 422
-        )
+        return {"message": "User already has this item or reuse url"}, 422
     item.user_id = new_user.id
     db.session.commit()
     result = item_schema.dump(Item.query.get(item.id))
-    return make_response(jsonify({"user": result}), 200)
+    return {"user": result}, 200
 
 
 @api_blueprint.route("/api/v1/user/registration", methods=["POST"])
@@ -175,15 +162,15 @@ def create_user() -> wrappers.Response:
     try:
         data = user_schema.load(json_data)
     except ValidationError as err:
-        return make_response(jsonify({"message": f"{ err.messages }"}), 422)
+        return {"message": f"{ err.messages }"}, 422
     username, password = data["username"], data["password"]
     user = User.query.filter_by(username=username).first()
     if user:
-        return make_response(jsonify({"message": "User already exist"}), 422)
+        return {"message": "User already exist"}, 422
     user = User(username=username, password=password)
     user.create()
     result = user_schema.dump(User.query.get(user.id))
-    return make_response(jsonify({"user": result}), 200)
+    return {"user": result}, 200
 
 
 @api_blueprint.route("/api/v1/user/login", methods=["POST"])
@@ -193,11 +180,11 @@ def login_user() -> wrappers.Response:
     try:
         data = user_schema.load(json_data)
     except ValidationError as err:
-        return make_response(jsonify({"message": f"{ err.messages }"}), 422)
+        return {"message": f"{ err.messages }"}, 422
     username, password = data["username"], data["password"]
     user = User.query.filter_by(username=username).first()
     if not user:
-        return make_response(jsonify({"message": "User is not exist"}), 422)
+        return {"message": "User is not exist"}, 422
     if user.verify_password(password):
         auth_token = jwt.encode(
             {
@@ -207,5 +194,5 @@ def login_user() -> wrappers.Response:
             },
             current_app.config["SECRET_KEY"],
         )
-        return make_response(jsonify({"user": {"auth_token": auth_token}}), 200)
-    return make_response(jsonify({"message": "Wrong password!"}), 403)
+        return {"user": {"auth_token": auth_token}}, 200
+    return {"message": "Wrong password!"}, 403
